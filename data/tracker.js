dc4f3ac8cbf9b951dc10adc8ec97a471c2129241
bin3743-debug <267347901+bin3743-debug@users.noreply.github.com>
2026-04-06 21:39:17 +0900

    Update KRX dashboard 2026-04-06 21:39:17

--- data/tracker.js
+++ data/tracker.js
@@ -1,5 +1,5 @@
 window.KRX_DASHBOARD_DATA = {
-  "generated_at": "2026-04-06 19:06:36",
+  "generated_at": "2026-04-06 21:39:16",
   "start_date": "2026-03-12",
   "latest_date": "2026-04-06",
   "rows": [
